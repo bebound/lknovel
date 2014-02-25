fa004810ce3504adb2f56d2d4757c022961fad02
bebound <bebound@gmail.com>
2014-02-25 14:05:59 +0800

    添加GUIVersion

---
+++ GUIVersion.py
@@ -0,0 +1,125 @@
+import sys
+import re
+import os
+import threading
+from PyQt4 import QtCore, QtGui
+import ui_mainWindow
+import ui_helpWidget
+import ui_aboutWidget
+import lk2epub
+
+
+class HelpWidget(QtGui.QDialog, ui_helpWidget.Ui_Dialog):
+    def __init__(self, parent=None):
+        QtGui.QDialog.__init__(self, parent)
+        self.setupUi(self)
+        self.setFixedSize(380, 190)
+
+        self.pushButton.clicked.connect(lambda: self.close())
+
+
+class AboutWidget(QtGui.QDialog, ui_aboutWidget.Ui_Dialog):
+    def __init__(self, parent=None):
+        QtGui.QDialog.__init__(self, parent)
+        self.setupUi(self)
+        self.setFixedSize(320, 120)
+
+        self.pushButton.clicked.connect(lambda: self.close())
+
+
+class MainWindow(QtGui.QMainWindow, ui_mainWindow.Ui_MainWindow):
+    sigWarningMessage = QtCore.pyqtSignal(str, str)
+    sigInformationMessage = QtCore.pyqtSignal(str, str)
+
+    def __init__(self, parent=None):
+        QtGui.QMainWindow.__init__(self, parent)
+        self.setupUi(self)
+        self.setFixedSize(520, 250)
+        self.setWindowTitle('lknovel-轻之国度在线轻小说转epub')
+        self.helpAction = QtGui.QAction('&Help', self)
+        self.helpAction.setStatusTip('使用说明')
+        self.aboutAction = QtGui.QAction('&About', self)
+        self.aboutAction.setStatusTip('关于')
+        self.menubar.addAction(self.helpAction)
+        self.menubar.addAction(self.aboutAction)
+        self.savePath = os.path.join(os.path.expanduser('~'), 'Desktop')
+        self.directoryLineEdit.setText(self.savePath)
+        self.coverPath = ''
+
+        self.startButton.clicked.connect(self.createEpub)
+        self.sigInformationMessage.connect(self.showInformationMessage)
+        self.sigWarningMessage.connect(self.showWarningMessage)
+        self.directoryButton.clicked.connect(self.selectSaveDirectory)
+        self.coverButton.clicked.connect(self.selectCover)
+        lk2epub.sender.sigChangeStatus.connect(self.changeStatus)
+        lk2epub.sender.sigWarningMessage.connect(self.showWarningMessage)
+        lk2epub.sender.sigDone.connect(self.workDone)
+        self.helpAction.triggered.connect(self.openHelpWidget)
+        self.aboutAction.triggered.connect(self.openAboutWidget)
+
+
+    def createEpub(self):
+        url = self.urlLineEdit.text()
+        ok = 0
+        check = re.compile(r'http://lknovel.lightnovel.cn/main/vollist/(\d+).html')
+        check2 = re.compile(r'http://lknovel.lightnovel.cn/main/book/(\d+).html')
+        if check.search(url) or check2.search(url):
+            ok = 1
+        if ok:
+            if url.split('/')[-2] == 'book':
+                t = threading.Thread(target=lk2epub.parseVolume, args=(url, self.savePath, self.coverPath))
+                t.start()
+            else:
+                t = threading.Thread(target=lk2epub.parseList, args=(url, self.savePath, self.coverPath))
+                t.start()
+            self.startButton.setEnabled(False)
+        else:
+            self.sigWarningMessage.emit('网址错误',
+                                        '请输入正确的网址，例如：\nhttp://lknovel.lightnovel.cn/main/vollist/726.html\nhttp://lknovel.lightnovel.cn/main/book/2664.html')
+
+
+    def selectSaveDirectory(self):
+        tempPath = str(QtGui.QFileDialog.getExistingDirectory(self, "选择文件夹"))
+        if tempPath:
+            self.savePath = tempPath
+            self.directoryLineEdit.setText(self.savePath)
+
+    def selectCover(self):
+        tempPath = str(
+            QtGui.QFileDialog.getOpenFileNameAndFilter(self, "选择文件", os.path.join(os.path.expanduser('~'), 'Desktop'),
+                                                       self.tr('图片文件(*.png *.jpg)'))[0])
+        if tempPath:
+            self.coverPath = tempPath
+            self.coverLineEdit.setText(self.coverPath)
+
+    def workDone(self):
+        self.changeStatus('')
+        self.sigInformationMessage.emit('完成', 'EPUB已生成')
+        self.startButton.setEnabled(True)
+
+    def openHelpWidget(self):
+        self.helpWidget = HelpWidget()
+        self.helpWidget.show()
+
+    def openAboutWidget(self):
+        self.aboutWidget = AboutWidget()
+        self.aboutWidget.show()
+
+    def changeStatus(self, text):
+        self.statusbar.showMessage(text)
+
+    def showWarningMessage(self, title, content):
+        QtGui.QMessageBox.warning(self, title, content, buttons=QtGui.QMessageBox.Ok,
+                                  defaultButton=QtGui.QMessageBox.NoButton)
+
+
+    def showInformationMessage(self, title, content):
+        QtGui.QMessageBox.information(self, title, content, buttons=QtGui.QMessageBox.Ok,
+                                      defaultButton=QtGui.QMessageBox.NoButton)
+
+
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+    lkMainWidndow = MainWindow()
+    lkMainWidndow.show()
+    app.exec_()
---
+++ ui_helpWidget.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'helpWidget.ui'
+#
+# Created: Tue Feb 25 13:47:06 2014
+#      by: PyQt4 UI code generator 4.10.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName(_fromUtf8("Dialog"))
+        Dialog.resize(377, 194)
+        self.verticalLayout_2 = QtGui.QVBoxLayout(Dialog)
+        self.verticalLayout_2.setMargin(0)
+        self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
+        self.verticalLayout = QtGui.QVBoxLayout()
+        self.verticalLayout.setSpacing(0)
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.textBrowser = QtGui.QTextBrowser(Dialog)
+        self.textBrowser.setObjectName(_fromUtf8("textBrowser"))
+        self.verticalLayout.addWidget(self.textBrowser)
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
+        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem)
+        self.pushButton = QtGui.QPushButton(Dialog)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Minimum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
+        self.pushButton.setSizePolicy(sizePolicy)
+        self.pushButton.setMinimumSize(QtCore.QSize(0, 30))
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.horizontalLayout.addWidget(self.pushButton)
+        spacerItem1 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem1)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.verticalLayout_2.addLayout(self.verticalLayout)
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(_translate("Dialog", "使用说明", None))
+        self.textBrowser.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'SimSun\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:12pt; font-weight:600;\">使用说明</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">1网址 输入轻小说在轻之国度的完整地址，系列或单卷均可，例如：</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">http://lknovel.lightnovel.cn/main/book/2664.html</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">http://lknovel.lightnovel.cn/main/vollist/726.html</p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">2封面 轻之国度自带的封面太小，可自定义轻小说封面</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">另外推荐下自己的另一个脚本<a href=\"https://github.com/bebound/scripts\"><span style=\" text-decoration: underline; color:#0000ff;\">bookwalker.py</span></a></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">可批量获取bookwalker上的轻小说封面</p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">3保存路径 默认为桌面</p></body></html>", None))
+        self.pushButton.setText(_translate("Dialog", "确定", None))
+
